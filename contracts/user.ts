79af5c5a5d8d19f6ed0106dc1965fcb4a5ddb731
Nemixe <rafel.ami@gmail.com>
2024-02-04 20:08:08 +0700

    init ts-rest

---
+++ contracts/user.ts
@@ -0,0 +1,35 @@
+// contract.ts
+
+import { initContract } from "@ts-rest/core";
+import { z } from "zod";
+
+const c = initContract();
+
+const PostSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  age: z.number(),
+});
+
+export const userContract = c.router({
+  createPost: {
+    method: "POST",
+    path: "/user",
+    responses: {
+      201: PostSchema,
+    },
+    body: z.object({
+      name: z.string(),
+      age: z.number(),
+    }),
+    summary: "Create a user",
+  },
+  getPost: {
+    method: "GET",
+    path: `/user/:id`,
+    responses: {
+      200: PostSchema.nullable(),
+    },
+    summary: "Get a user by id",
+  },
+});
